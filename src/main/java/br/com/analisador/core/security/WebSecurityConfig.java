3c727d24a2e2ce2fc75ca2769adc0b18fca40ef1
matheus5ilva <matheusxandre4@gmail.com>
2023-06-13 19:15:15 -0300

    Alteração das rotas da API.

--- src/main/java/br/com/analisador/core/security/WebSecurityConfig.java
+++ src/main/java/br/com/analisador/core/security/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -21,26 +22,45 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .httpBasic()
-                .and()
                 .authorizeRequests()
                 .antMatchers("/h2-console/**").permitAll()
-                .antMatchers(HttpMethod.GET,"/api/empresas/**").hasAnyRole("ADMIN", "USER")
-                .antMatchers(HttpMethod.POST,"/api/empresas/**").hasRole("ADMIN")
-                .antMatchers(HttpMethod.DELETE,"/api/empresas/**").hasRole("ADMIN")
-                .antMatchers(HttpMethod.PUT,"/api/empresas/**").hasAnyRole("ADMIN", "USER")
-                .antMatchers(HttpMethod.GET,"/api/usuarios/**").hasAnyRole("ADMIN", "USER", "CUSTOM")
-                .antMatchers(HttpMethod.POST,"/api/usuarios/**").hasAnyRole("ADMIN", "USER")
-                .antMatchers(HttpMethod.PUT,"/api/usuarios/**").hasAnyRole("ADMIN", "USER", "CUSTOM")
-                .antMatchers(HttpMethod.DELETE,"/api/usuarios/**").hasAnyRole("ADMIN", "USER")
-                .antMatchers(HttpMethod.GET,"/api/pesquisas/**").hasAnyRole("ADMIN", "USER", "CUSTOM")
-                .antMatchers(HttpMethod.POST,"/api/pesquisas/**").hasAnyRole("ADMIN", "USER", "CUSTOM")
+                .antMatchers(HttpMethod.GET, "/api/empresas/**").hasAnyRole("ADMIN", "USER")
+                .antMatchers(HttpMethod.POST, "/api/empresas/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/api/empresas/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT, "/api/empresas/**").hasAnyRole("ADMIN", "USER")
+                .antMatchers(HttpMethod.GET, "/api/usuarios/**").hasAnyRole("ADMIN", "USER", "CUSTOM")
+                .antMatchers(HttpMethod.POST, "/api/usuarios/**").hasAnyRole("ADMIN", "USER")
+                .antMatchers(HttpMethod.PUT, "/api/usuarios/**").hasAnyRole("ADMIN", "USER", "CUSTOM")
+                .antMatchers(HttpMethod.DELETE, "/api/usuarios/**").hasAnyRole("ADMIN", "USER")
+                .antMatchers(HttpMethod.GET, "/api/pesquisas/**").hasAnyRole("ADMIN", "USER", "CUSTOM")
+                .antMatchers(HttpMethod.POST, "/api/pesquisas/**").hasAnyRole("ADMIN", "USER", "CUSTOM")
+                .antMatchers(HttpMethod.GET, "/").hasAnyRole("ADMIN", "USER", "CUSTOM")
+                .antMatchers(HttpMethod.PUT, "/empresa/**").hasAnyRole("ADMIN", "USER")
+                .antMatchers(HttpMethod.GET, "/usuarios/**").hasAnyRole("ADMIN", "USER", "CUSTOM")
+                .antMatchers(HttpMethod.POST, "/usuarios/**").hasAnyRole("ADMIN", "USER")
+                .antMatchers(HttpMethod.PUT, "/usuarios/**").hasAnyRole("ADMIN", "USER", "CUSTOM")
+                .antMatchers(HttpMethod.DELETE, "/usuarios/**").hasAnyRole("ADMIN", "USER")
+                .antMatchers(HttpMethod.GET, "/pesquisas/**").hasAnyRole("ADMIN", "USER", "CUSTOM")
+                .antMatchers(HttpMethod.POST, "/pesquisas/**").hasAnyRole("ADMIN", "USER", "CUSTOM")
                 .anyRequest().authenticated()
                 .and()
-                .headers().frameOptions().disable()
+                .httpBasic()
+                .realmName("API Realm") // Nome do reino para autenticação Basic
+                .and()
+                .formLogin()
+                .loginPage("/login") // Define a página de login personalizada
+                .defaultSuccessUrl("/", true)
+                .permitAll()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/login")
+                .permitAll()
+                .and()
+                .headers().frameOptions().disable() // Permite o acesso ao H2 Console
                 .and()
                 .csrf().disable();
     }
+
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -57,4 +77,3 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 }
-
